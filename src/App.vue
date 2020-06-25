cf5119a047e58e6ef3fef1c0282be67ea0ed3688
james-d-f <james.f@brainlabsdigital.com>
2020-06-25 14:23:41 +0100

    Add basic style and layout

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,25 @@
     <v-app-bar app color="primary" dark>Capital</v-app-bar>
 
     <v-content>
-      <v-card>
-        <v-card-title>What's the capital?</v-card-title>
-        <v-card-text>
-          <h2>{{currentCountry["name"]}}</h2>
-          <v-text-field v-model="guess"></v-text-field>
-          <v-btn @click="submitGuess">Submit answer</v-btn>
-          <p>{{previousAnswerMessage}}</p>
-          <h3>Current score:</h3>
-          <p>{{score}} / {{attempts}}</p>
-        </v-card-text>
-      </v-card>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="pa-5 rounded-xl">
+              <v-card-title class="mb-8">
+                <h1>What's the capital?</h1>
+              </v-card-title>
+              <v-card-text>
+                <h2 class="mb-8">{{currentCountry["name"]}}</h2>
+                <v-text-field style="max-width: 250px;" label="Guess" v-model="guess"></v-text-field>
+                <v-btn color="primary" @click="submitGuess">Submit answer</v-btn>
+                <p>{{previousAnswerMessage}}</p>
+                <h3 class="mt-8">Current score:</h3>
+                <p>{{score}} / {{attempts}}</p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
